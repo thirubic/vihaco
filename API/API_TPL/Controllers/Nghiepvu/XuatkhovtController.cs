21f549ceee2a90098deb460bbe92770cee0f049a
unknown <tranvanthi1993@gmail.com>
2024-01-03 15:24:16 +0700

    them moi api xuat kho 15h24 03/01/2024

---
+++ API/API_TPL/Controllers/Nghiepvu/XuatkhovtController.cs
@@ -0,0 +1,93 @@
+﻿using API_TPL.DAL;
+using Microsoft.AspNet.Identity;
+using Oracle.ManagedDataAccess.Client;
+using System.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Web;
+using System.Web.Http;
+
+namespace API_TPL.Controllers.Nghiepvu
+{
+    [RoutePrefix("api/xuatkhovt")]
+    public class XuatkhovtController : ApiController
+    {
+        static String connString = ConfigurationManager.ConnectionStrings["GSkhoConnection"].ToString();
+        SQL_DBHELPERs helper = new SQL_DBHELPERs(connString);
+        [Route("xuatkho"), HttpPost]
+        public IHttpActionResult xuatkho_insert([FromBody] dynamic obj)
+        {
+            string query_str = "xuatkho_insert";
+
+            object[] aParams = new object[6];
+            try
+            {
+                aParams[0] = helper.BuildParameter("NGAYXUATKHO", obj.NGAYXUATKHO, System.Data.SqlDbType.NVarChar);
+                aParams[1] = helper.BuildParameter("SOHOADON", obj.SOHOADON, System.Data.SqlDbType.NVarChar);
+                aParams[2] = helper.BuildParameter("MAKHO", obj.MAKHO, System.Data.SqlDbType.NVarChar);
+                aParams[3] = helper.BuildParameter("IDANH", obj.IDANH, System.Data.SqlDbType.NVarChar);
+                aParams[4] = helper.BuildParameter("GHICHU", obj.GHICHU, System.Data.SqlDbType.NVarChar);
+                aParams[5] = helper.BuildParameter("data", obj.data, System.Data.SqlDbType.NVarChar);
+
+                DataTable kq = helper.ExecuteQueryStoreProcedure(query_str, aParams);
+
+                return ResponseMessage(Request.CreateResponse(HttpStatusCode.OK, kq));
+            }
+            catch (Exception ex)
+            {
+                return ResponseMessage(Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ex.Message));
+            }
+        }
+        [Route("update"), HttpPost]
+        public IHttpActionResult xuatkho_update([FromBody] dynamic obj)
+        {
+            string query_str = "xuatkho_update";
+            //test git
+            object[] aParams = new object[7];
+            try
+            {
+                aParams[0] = helper.BuildParameter("IDXUATKHO", obj.IDXUATKHO, System.Data.SqlDbType.NVarChar);
+                aParams[1] = helper.BuildParameter("NGAYXUATKHO", obj.NGAYXUATKHO, System.Data.SqlDbType.NVarChar);
+                aParams[2] = helper.BuildParameter("SOHOADON", obj.SOHOADON, System.Data.SqlDbType.NVarChar);
+                aParams[3] = helper.BuildParameter("MAKHO", obj.MAKHO, System.Data.SqlDbType.NVarChar);
+                aParams[4] = helper.BuildParameter("IDANH", obj.IDANH, System.Data.SqlDbType.NVarChar);
+                aParams[5] = helper.BuildParameter("GHICHU", obj.GHICHU, System.Data.SqlDbType.NVarChar);
+                aParams[6] = helper.BuildParameter("data", obj.data, System.Data.SqlDbType.NVarChar);
+
+                DataTable kq = helper.ExecuteQueryStoreProcedure(query_str, aParams);
+
+                return ResponseMessage(Request.CreateResponse(HttpStatusCode.OK, kq));
+            }
+            catch (Exception ex)
+            {
+                return ResponseMessage(Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ex.Message));
+            }
+        }
+        [Route("xoa"), HttpPost]
+        public IHttpActionResult xuatkho_delete([FromBody] dynamic obj)
+        {
+            string query_str = "xuatkho_delete";
+
+            object[] aParams = new object[1];
+            try
+            {
+                aParams[0] = helper.BuildParameter("IDXUATKHO", obj.IDXUATKHO, System.Data.SqlDbType.NVarChar);
+
+                DataTable kq = helper.ExecuteQueryStoreProcedure(query_str, aParams);
+
+                return ResponseMessage(Request.CreateResponse(HttpStatusCode.OK, kq));
+            }
+            catch (Exception ex)
+            {
+                return ResponseMessage(Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ex.Message));
+            }
+        }
+    }
+}
