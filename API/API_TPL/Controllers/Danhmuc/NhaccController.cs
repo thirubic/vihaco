fdbee87547e9885d46aebe1afda38540f9c463ce
unknown <tranvanthi1993@gmail.com>
2024-01-10 16:07:25 +0700

    Dua them 2 controler api cua nhacc va loainhancong 16h07 10/10/2024

---
+++ API/API_TPL/Controllers/Danhmuc/NhaccController.cs
@@ -0,0 +1,120 @@
+﻿using API_TPL.DAL;
+using Microsoft.AspNet.Identity;
+using Oracle.ManagedDataAccess.Client;
+using System.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Web;
+using System.Web.Http;
+
+namespace API_TPL.Controllers.Danhmuc
+{
+    [RoutePrefix("api/nhacc")]
+    public class NhaccController : ApiController
+    {
+        static String connString = ConfigurationManager.ConnectionStrings["GSkhoConnection"].ToString();
+        SQL_DBHELPERs helper = new SQL_DBHELPERs(connString);
+        [HostAuthentication(DefaultAuthenticationTypes.ExternalBearer)]
+        [Route("getall"), HttpGet]
+        public IHttpActionResult getAll()
+        {
+            string query_str = "nhacc_getall";
+
+            object[] aParams = new object[0];
+            try
+            {
+                DataTable kq = helper.ExecuteQueryStoreProcedure(query_str, aParams);
+
+                return ResponseMessage(Request.CreateResponse(HttpStatusCode.OK, kq));
+            }
+            catch (Exception ex)
+            {
+                return ResponseMessage(Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ex.Message));
+            }
+        }
+        [Route("insert"), HttpPost]
+        public IHttpActionResult nhacc_insert([FromBody] dynamic obj)
+        {
+            string query_str = "nhacc_insert";
+
+            object[] aParams = new object[1];
+            try
+            {
+                aParams[0] = helper.BuildParameter("data", obj.data, System.Data.SqlDbType.NVarChar);
+
+                DataTable kq = helper.ExecuteQueryStoreProcedure(query_str, aParams);
+
+                return ResponseMessage(Request.CreateResponse(HttpStatusCode.OK, kq));
+            }
+            catch (Exception ex)
+            {
+                return ResponseMessage(Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ex.Message));
+            }
+        }
+        [Route("update"), HttpPost]
+        public IHttpActionResult loainhancong_update([FromBody] dynamic obj)
+        {
+            string query_str = "nhacc_update";
+
+            object[] aParams = new object[1];
+            try
+            {
+                aParams[0] = helper.BuildParameter("data", obj.data, System.Data.SqlDbType.NVarChar);
+
+                DataTable kq = helper.ExecuteQueryStoreProcedure(query_str, aParams);
+
+                return ResponseMessage(Request.CreateResponse(HttpStatusCode.OK, kq));
+            }
+            catch (Exception ex)
+            {
+                return ResponseMessage(Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ex.Message));
+            }
+        }
+        [Route("delete"), HttpPost]
+        public IHttpActionResult loainhancong_delete([FromBody] dynamic obj)
+        {
+            string query_str = "nhacc_delete";
+
+            object[] aParams = new object[1];
+            try
+            {
+                aParams[0] = helper.BuildParameter("IDNCC", obj.IDNCC, System.Data.SqlDbType.Int);
+
+                DataTable kq = helper.ExecuteQueryStoreProcedure(query_str, aParams);
+
+                return ResponseMessage(Request.CreateResponse(HttpStatusCode.OK, kq));
+            }
+            catch (Exception ex)
+            {
+                return ResponseMessage(Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ex.Message));
+            }
+        }
+        [Route("getbyid"), HttpPost]
+        public IHttpActionResult loainhancong_getbyid([FromBody] dynamic obj)
+        {
+            string query_str = "nhacc_getbyid";
+
+            object[] aParams = new object[1];
+            try
+            {
+                aParams[0] = helper.BuildParameter("IDNCC", obj.IDNCC, System.Data.SqlDbType.Int);
+
+                DataTable kq = helper.ExecuteQueryStoreProcedure(query_str, aParams);
+
+                return ResponseMessage(Request.CreateResponse(HttpStatusCode.OK, kq));
+            }
+            catch (Exception ex)
+            {
+                return ResponseMessage(Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ex.Message));
+            }
+        }
+
+    }
+}
